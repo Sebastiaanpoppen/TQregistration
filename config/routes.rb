e8261c3394c52990b07c88cc381e974e2c4a7767
Sebastiaan Poppen <sp.poppen@quicknet.nl>
2016-10-24 11:47:03 +0200

    controller home

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  root to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
