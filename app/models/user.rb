c3150926f0721146038b1987d65fc48bd74c7c08
adrianabm <adrianabm@gmail.com>
2016-10-24 16:40:24 +0200

    styling and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+
 
   def full_name
     "#{first_name} #{last_name}"
   end
-  
+
 end
