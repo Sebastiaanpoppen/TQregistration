9c881eaef3a4d15f5599ca03f3a2ea92dd4a9076
giuliogallerini <giulio.gallerini.work.com>
2016-10-24 18:17:33 +0200

    fix bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || admin_users_path(current_admin)
+    request.env['omniauth.origin'] || stored_location_for(resource) || admin_users_path(current_admin.id)
   end
 end
