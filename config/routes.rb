dd38e2380f3d599412df62ad6aefc268918adcf1
adrianabm <adrianabm@gmail.com>
2016-10-24 13:02:04 +0200

    added registration form partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'pages/home'
   root to: 'pages#home'
 
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
