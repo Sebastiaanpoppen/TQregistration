ae25f1ee0414b8d4fdfc3db149d1620ca6be25d1
Sebastiaan Poppen <sp.poppen@quicknet.nl>
2016-10-25 10:19:48 +0200

    installed factories

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  it "should get new" do
+      get :new
+      expect(response).to be_success
+      expect(assigns(:users)).to eq(nil)
+    end
 
 end
