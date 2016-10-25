3973722b903976348e778ea09dc88870e917f3c4
adrianabm <adrianabm@gmail.com>
2016-10-25 15:38:41 +0200

    add tq logo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,5 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || stored_location_for(resource) || admin_users_path(current_admin.id)
   end
+
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,9 @@
 module PagesHelper
+
+  def svg(name)
+    file_path = "#{Rails.root}/app/assets/images/tq-diamond.svg"
+    return File.read(file_path).html_safe if File.exists?(file_path)
+    '(not found)'
+  end
+  
 end
