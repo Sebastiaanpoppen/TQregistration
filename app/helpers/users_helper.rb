5ffc3501d5d759a75b1d7cd222b00758db034f13
giuliogallerini <giulio.gallerini.work.com>
2016-10-28 13:37:00 +0200

    restyled index users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,4 +7,13 @@ module UsersHelper
   def user_company user
     !user.company.nil? ? user.company : ""
   end
+
+  def last_checkin user
+    @booking = user.bookings.where('checkin <= ?', Date.today).first
+    if @booking.blank?
+      "never checked in yet"
+    else
+      @booking.checkin.to_date
+    end
+  end
 end
