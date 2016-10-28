a86b435d8e280ee8d50538c3b79e07cad154a5a6
Sebastiaan Poppen <sp.poppen@quicknet.nl>
2016-10-28 12:16:33 +0200

    working create function

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,21 +1,29 @@
-class AdminsController < ApplicationController
-
-  def new
-    @admin = Admin.new
-  end
+class AdminsController < Devise::RegistrationsController
+  before_filter :authorize_admin, only: [:create, :destroy]
+  skip_before_action :require_no_authentication, only: :new
 
   def create
-    @admin = Admin.create(admin_params)
-      if @admin.save
-          redirect_to admin_bookings_path
-      else
-        render :new
+    if Admin.create(admin_params)
+      redirect_to "/admin", notice: "New admin succesfully created"
+    else
+      redirect_to "/admin" , alert: "There was an error creating the new admin"
     end
   end
 
-end
+  def sign_up(resource_name, resource)
+    true
+  end
+  private
 
-private
+  # This should probably be abstracted to ApplicationController
+  # as shown by diego.greyrobot
+  def authorize_admin
+    return unless !current_admin.super_admin?
+    redirect_to admin_bookings_path, alert: 'Admins only!'
+  end
+
   def admin_params
     params.require(:admin).permit(:email, :password)
   end
+
+end
