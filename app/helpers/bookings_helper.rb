ec352581395adf57d5020b66eb6f0574f2f0b5b6
giuliogallerini <giulio.gallerini.work.com>
2016-11-21 15:28:27 +0100

    display chart data relative to the admin access permitions

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -7,4 +7,20 @@ module BookingsHelper
   def admin booking
     booking.admin.blank? ? nil : booking.admin.id
   end
+
+  def chart_data admin, type
+    case type
+    when 'day'
+      (!admin.full_access.blank? && admin.full_access) || admin.super_admin ?
+        Booking.group_by_day(:checkin, format: "%b %d, %Y", library: {yAxis: {allowDecimals: false}}).count : @admin.bookings.group_by_day(:checkin, format: "%b %d, %Y", library: {yAxis: {allowDecimals: false}}).count
+    when 'month'
+      (!admin.full_access.blank? && admin.full_access) || admin.super_admin ?
+      Booking.group_by_month(:checkin, format: "%b", library: {yAxis: {allowDecimals: false}}).count : @admin.bookings.group_by_month(:checkin, format: "%b", library: {yAxis: {allowDecimals: false}}).count
+    when 'year'
+      (!admin.full_access.blank? && admin.full_access) || admin.super_admin ?
+        Booking.group_by_year(:checkin, format: "%Y", library: {yAxis: {allowDecimals: false}}).count : @admin.bookings.group_by_year(:checkin, format: "%Y", library: {yAxis: {allowDecimals: false}}).count
+    end
+
+
+  end
 end
