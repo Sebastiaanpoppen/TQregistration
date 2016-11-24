e36bb659fe9ac74f068029830e42031ece561d70
giuliogallerini <giulio.gallerini.work@hotmail.com>
2016-11-24 11:17:03 +0100

    add active and full_access toogle for super admin

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,16 +1,35 @@
 $(document).ready(() => {
-  $(".active-check").bind('change',function(){
-    console.log(this.checked)
-    if(this.checked){
-      $.ajax({
-        url: window.location.href+"/toggle_active/"+this.value,
-        type: 'POST',
-        data: {"active": this.checked}
-      })
-      .fail(console.log)
-    }
-    else {
-       alert("no");
-    }
+  $(".active-check").bind('change',function() {
+    $.ajax({
+      type: 'PATCH',
+      url: window.location.origin + "/admins/toggle_active/" + this.value,
+      data: JSON.stringify({
+        "active": this.checked
+      }),
+      processData: false,
+      contentType: "application/json",
+      dataType: "json"
+    })
+    .done(function(data) {
+      console.log(data);
+    })
+    .fail(console.log)
+  });
+
+  $(".full-access-check").bind('change',function() {
+    $.ajax({
+      type: 'PATCH',
+      url: window.location.origin + "/admins/toggle_full_access/" + this.value,
+      data: JSON.stringify({
+        "full_access": this.checked
+      }),
+      processData: false,
+      contentType: "application/json",
+      dataType: "json"
+    })
+    .done(function(data) {
+      console.log(data);
+    })
+    .fail(console.log)
   });
 });
