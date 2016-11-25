de195994e0c7b3f80de229666f219e741164604a
giuliogallerini <giulio.gallerini.work@hotmail.com>
2016-11-25 15:45:52 +0100

    changed configuration for emails notification

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV["EMAIL_FROM"] || 'from@example.com'
+  default from: ENV["EMAIL_FROM"] || "TQ Registration <hello@tq.co>"
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,6 +95,5 @@ Rails.application.configure do
     password: ENV["SMTP_PASSWORD"],
     authentication: 'login',
     enable_starttls_auto: true,
-    openssl_verify_mode: 'none'
   }
 end
