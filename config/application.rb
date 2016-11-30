6b87fb17ba715eb5b9646701f8fc817bd460b128
Sebastiaan Poppen <sp.poppen@quicknet.nl>
2016-11-30 11:42:47 +0100

    changed timezones

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,9 @@ module Tqregistration
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.time_zone = 'Amsterdam'
+    config.active_record.default_timezone = :local
+
   end
 end
